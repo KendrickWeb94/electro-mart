986b8a01e33e2b78209d3e67a7dd275a640e77c1
Kendrick Victor <sunflowerroses801@gmail.com>
2024-06-10 22:03:04 +0000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/themes/themeprovider";
+import { ModeToggle } from "./components/themes/modetoggle";
+
+interface AppProps {
+  children: ReactNode;
+}
+
+function App({ children }: AppProps) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className=" w-full relative">
+        <div className="">
+          <ModeToggle />
+        </div>
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
