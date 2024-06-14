29f06e10a4bcffaa18933840bab6894949a1f43a
Kendrick Victor <sunflowerroses801@gmail.com>
2024-06-14 04:06:53 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { ReactNode } from "react";
-import { ThemeProvider } from "@/components/themes/themeprovider";
+
 import { ModeToggle } from "./components/themes/modetoggle";
 
-interface AppProps {
-  children: ReactNode;
-}
-
-function App({ children }: AppProps) {
+function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+   
       <div className=" w-full relative dark:bg-slate-950">
         <div className="">
           <ModeToggle />
         </div>
-        {children}
       </div>
-    </ThemeProvider>
+
   );
 }
 
