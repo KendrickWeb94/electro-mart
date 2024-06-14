9c2f647a2e920c86f168b11300dc243b8ad09e9d
Kendrick Victor <sunflowerroses801@gmail.com>
2024-06-14 04:06:00 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/themes/themeprovider";
+
+interface AppProps {
+  children: ReactNode;
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
