ee33657a92594f55222b6ab430b27accfaf39dc5
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-10-09 14:24:40 -0700

    bruh

--- src/Routes/404.tsx
+++ src/Routes/404.tsx
@@ -1,16 +1,14 @@
-
-import notfound from "../assets/imgs/404.svg"
-
-
+import notfound from "../assets/imgs/404.svg";
 
 const NotFound = () => {
   return (
     <div className="w-full  h-screen flex items-center justify-center">
-        <div className="space-y-6">
-         hey guysss
-        </div>
+      <div className="space-y-6">
+        <img src={notfound} alt="" className="w-[300px]" />
+        helo
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
